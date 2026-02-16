ae1b57e0eae3344114f5598d10450e47c13d83b7
Artem Novikov <artemnovikov@Artems-Mac-mini.local>
2026-02-16 06:43:40 +0500

    feat(hw02): basic string's unpack logic of the func Unpack() implementation

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,67 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(inputStr string) (string, error) {
+	if inputStr == "" {
+		return "", nil
+	}
+
+	var builder strings.Builder
+
+	var prevRune rune
+	var hasPrev bool = false
+	var prevWasDigit bool = false
+
+	flushPrev := func() {
+		if hasPrev {
+			builder.WriteRune(prevRune)
+			hasPrev = false
+		}
+	}
+
+	for _, r := range inputStr {
+		if unicode.IsDigit(r) {
+
+			if !hasPrev {
+				prevWasDigit = false
+				continue
+			}
+
+			if prevWasDigit {
+				return "", ErrInvalidString
+			}
+
+			count, err := strconv.Atoi(string(r))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+
+			if count == 0 {
+				hasPrev = false
+			} else {
+				builder.WriteRune(prevRune)
+				if count-1 > 0 {
+					builder.WriteString(strings.Repeat(string(prevRune), count-1))
+				}
+				hasPrev = false
+			}
+			prevWasDigit = true
+			continue
+		}
+
+		flushPrev()
+		prevRune = r
+		hasPrev = true
+		prevWasDigit = false
+
+	}
+	flushPrev()
+	return builder.String(), nil
 }
