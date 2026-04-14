4b02e4dfb38308c54f755055650b599081654d21
TommyBlackrock <artemus.novikov@gmail.com>
2026-04-14 22:41:17 +0500

    feat(hw07): file copying util implementation [skip ci]

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,81 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrNegativeOffset        = errors.New("negative offset is not allowed")
+	ErrNegativeLimit         = errors.New("negative limit is not allowed")
+	ErrSameFile              = errors.New("source and destination are the same file")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	if offset < 0 {
+		return ErrNegativeOffset
+	}
+	if limit < 0 {
+		return ErrNegativeLimit
+	}
+
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, 0)
+	if err != nil {
+		return fmt.Errorf("open source file: %w", err)
+	}
+	defer src.Close()
+
+	srcInfo, err := src.Stat()
+	if err != nil {
+		return fmt.Errorf("stat source file: %w", err)
+	}
+	if !srcInfo.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+
+	srcSize := srcInfo.Size()
+	if offset > srcSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	dstInfo, err := os.Stat(toPath)
+	switch {
+	case err == nil:
+		if os.SameFile(srcInfo, dstInfo) {
+			return ErrSameFile
+		}
+	case errors.Is(err, os.ErrNotExist):
+		// Destination file does not exist yet: this is a valid case.
+	default:
+		return fmt.Errorf("stat destination file: %w", err)
+	}
+
+	bytesToCopy := srcSize - offset
+	if limit > 0 && limit < bytesToCopy {
+		bytesToCopy = limit
+	}
+
+	dst, err := os.OpenFile(toPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+	if err != nil {
+		return fmt.Errorf("create destination file: %w", err)
+	}
+	defer dst.Close()
+
+	if _, err := src.Seek(offset, io.SeekStart); err != nil {
+		return fmt.Errorf("seek to offset %d: %w", offset, err)
+	}
+
+	copied, err := io.CopyN(dst, src, bytesToCopy)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("copy data: %w", err)
+	}
+
+	if copied != bytesToCopy {
+		return fmt.Errorf("copied %d bytes, expected to copy %d bytes", copied, bytesToCopy)
+	}
+
 	return nil
 }
--- hw07_file_copying/main.go
+++ hw07_file_copying/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
+	"os"
 )
 
 var (
@@ -18,5 +21,37 @@ func init() {
 
 func main() {
 	flag.Parse()
-	// Place your code here.
+	if from == "" {
+		fmt.Println("Error: 'from' flag is required")
+		os.Exit(1)
+	}
+	if to == "" {
+		fmt.Println("Error: 'to' flag is required")
+		os.Exit(1)
+	}
+
+	err := Copy(from, to, offset, limit)
+	if err == nil {
+		fmt.Printf("File %s copied successfully to %s\n", from, to)
+		return
+	}
+	switch {
+	case errors.Is(err, ErrNegativeOffset):
+		fmt.Fprintln(os.Stderr, "Error: offset cannot be negative")
+	case errors.Is(err, ErrNegativeLimit):
+		fmt.Fprintln(os.Stderr, "Error: limit cannot be negative")
+	case errors.Is(err, ErrOffsetExceedsFileSize):
+		fmt.Fprintln(os.Stderr, "Error: offset exceeds file size")
+	case errors.Is(err, ErrSameFile):
+		fmt.Fprintln(os.Stderr, "Error: source and destination must be different files")
+	case errors.Is(err, ErrUnsupportedFile):
+		fmt.Fprintln(os.Stderr, "Error: only regular files are supported")
+	case errors.Is(err, os.ErrNotExist):
+		fmt.Fprintf(os.Stderr, "Error: source file %s does not exist\n", from)
+	case errors.Is(err, os.ErrPermission):
+		fmt.Fprintf(os.Stderr, "Error: permission denied (reading %s or writing %s)\n", from, to)
+	default:
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+	}
+	os.Exit(1)
 }
